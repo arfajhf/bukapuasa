fda6c1be960106623de216d1e17b691e09f41366
arfajhf <fjrhdytlh22@gmail.com>
2024-11-17 17:18:09 +0700

    push

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('playAudio').addEventListener('click', function() {
+    const audio = new Audio('https://www.bensound.com/bensound-music/bensound-happyrock.mp3');
+    audio.play();
+});
